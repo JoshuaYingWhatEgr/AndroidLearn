7577593ba02749dae2464e7d880cd59c4855c709
joshuayingwhat <17721354011@163.com>
2019-04-10 14:58:13 +0800

    Initial commit

---
+++ app/src/main/java/com/joshuayingwhat/androidlearn/MainActivity.java
@@ -0,0 +1,17 @@
+package com.joshuayingwhat.androidlearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 这个是一个android知识的测试项目
+ * @author joshuayingwhat
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
